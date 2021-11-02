530d0d16cdcc9779eaa348510b66b303abdbd80a
Kaushal Joshi <7joshikaushal@gmail.com>
2021-11-03 01:39:08 +0530

    configured eslint, prettier, airbnb and husky

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App ">
-      <h1>
-        Hello World    
-      </h1>
+      <h1>Hello World</h1>
+      <h2>Style Guide Test</h2>
     </div>
   );
 }
-
 export default App;
